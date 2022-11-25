ae820ca01bbfa671eea1d2b31d95359528b2c8d6
GuiCMartins <guilhermecmartins99@gmail.com>
2022-11-25 20:06:41 -0300

    CRUD

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -7,7 +7,57 @@ const useLivrosController = () => {
     });
   };
 
-  return { get };
+  const getById = (req, res) => {
+    const id = req.params.id;
+
+    livros.findById(id, (err, livro) => {
+      if (!err) {
+        res.status(200).send(livro);
+      } else {
+        res.status(404).send({ message: `${err.message} - Id não encontrado` });
+      }
+    });
+  };
+
+  const post = (req, res) => {
+    const livro = new livros(req.body);
+
+    livro.save((err) => {
+      if (!err) {
+        res.status(201).send(livro.toJSON());
+      } else {
+        res
+          .status(500)
+          .send({ message: `${err.message} - falha ao cadastrar livro` });
+      }
+    });
+  };
+
+  const put = (req, res) => {
+    const id = req.params.id;
+
+    livros.findByIdAndUpdate(id, { $set: req.body }, (err) => {
+      if (!err) {
+        res.status(200).send({ message: 'Livro atualizado com sucesso' });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+  };
+
+  const remove = (req, res) => {
+    const id = req.params.id;
+
+    livros.findByIdAndDelete(id, (err) => {
+      if (!err) {
+        res.status(200).send({ message: 'Livro removido com sucesso' });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+  };
+
+  return { get, getById, post, put, remove };
 };
 
 export default useLivrosController;
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -4,6 +4,13 @@ import useLivrosController from '../controllers/livrosController.js';
 const router = express.Router();
 const livrosController = useLivrosController();
 
-router.get('/livros', livrosController.get);
+const url = '/livros';
+
+router
+  .get(url, livrosController.get)
+  .get(`${url}/:id`, livrosController.getById)
+  .post(url, livrosController.post)
+  .put(`${url}/:id`, livrosController.put)
+  .delete(`${url}/:id`, livrosController.remove);
 
 export default router;
